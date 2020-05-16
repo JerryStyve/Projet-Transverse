afe737e59bae22f102a18bfc0188f1b718813d46
emilio1005 <53258646+emilio1005@users.noreply.github.com>
2020-05-16 11:33:41 +0200

    Add files via upload
    It's just a version 1. I didn't able to make events, because i've had many errors. So let's see this version, if you can help me it'll a great pleasure. Please
    you'll see that the file index of the pageweb is one that have php code.
    i'll continue to work it on

---
+++ views/header.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/calendrier.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-primary mb-3">
+    <a href="/index.php" class="navbar-brand">Mes rendez-vous</a>
+</nav>
